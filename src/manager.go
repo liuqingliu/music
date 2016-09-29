7447eb850dd8f7d7ff45d0d7b7018c128ba0f8d3
liuqing@bang.360.cn <liuqing@bang.360.cn>
2016-09-29 17:45:47 +0800

    修改

--- src/manager.go
+++ src/manager.go
@@ -1,55 +1,54 @@
-package library
+package library 
+import "errors" 
 
-type MusicManager struct {
-	musics []MusicEntry
-}
+type MusicManager struct{ 
+    musics []MusicEntry 
+} 
 
-func NewMusicManager() *MusicManager {
-	return &MusicManager{make([]MusicEntry, 0)}
-}
+func NewMusicManager() *MusicManager { 
+    return &MusicManager{make([]MusicEntry, 0)} 
+} 
 
-func (m *MusicManager) Len() int {
-	return len(m.musics)
-}
+func (m *MusicManager) Len() int{ 
+    returnlen(m.musics) 
+} 
 
-func (m *MusicManager) Get(index int) (music *MusicEntry, err error) {
-	if index < 0 || index >= len(m.musics) {
-		return &m.musics[index], nil
-	}
-}
+func (m *MusicManager) Get(index int) (music *MusicEntry, err error) { 
+    if index < 0 || index >= len(m.musics) { 
+        return nil, errors.New("Index out of range.") 
+    } 
+    return  &m.musics[index], nil
+} 
 
-func (m *MusicManager) Find(name string) *MusicEntry {
-	if len(m.musics) == 0 {
-		return nil
-	}
+func (m *MusicManager) Find(name string) *MusicEntry { 
+    if len(m.musics) == 0 { 
+        return nil
+    } 
 
-	for _, m := range m.musics {
-		if m.Name == name {
-			return &m
-		}
-	}
+    for_, m := rangem.musics { 
+        if m.Name == name { 
+            return &m 
+        }
+    }    
+    return nil
+} 
 
-	return nil
-}
+func (m *MusicManager) Add(music *MusicEntry) { 
+    m.musics = append(m.musics, *music) 
+} 
 
-func (m *MusicManager) Add(music *MusicEntry) {
-	m.musics = append(m.musics, *music)
-}
-
-func (m *MusicManager) Remove(index int) *MusicEntry {
-	if index < 0 || index >= len(m.musics) {
-		return nil
-	}
-
-	removedMusic := &m.musics[index]
-
-	if index < len(m.musics)-1 {
-		m.musics = append(m.musics[:index-1], m.musics[index+1:]...)
-	} else if index == 0 {
-		m.musics = make([]MusicEntry, 0)
-	} else {
-		m.musics = m.musics[:index-1]
-	}
-
-	return removedMusic
-}
+func (m *MusicManager) Remove(index int) *MusicEntry { 
+    if index < 0 || index >= len(m.musics) { 
+        return nil
+    } 
+    removedMusic := &m.musics[index] 
+    // 从数组切片中删除元素
+    if index <len(m.musics)-1 { // 中间元素
+        m.musics = append(m.musics[:index-1],m.musics[index+1:]...) 
+    } elseif index == 0 { // 删除仅有的一个元素
+        m.musics = make([]MusicEntry, 0) 
+    } else{ // 删除的是最后一个元素
+        m.musics = m.musics[:index-1] 
+    } 
+    returnremovedMusic
+} 
